bf1e8e795098f5ac352433a1761b4aa50ad67e13
ty262875992-bit <ty262875992@gmail.com>
2026-04-17 14:43:26 +0800

    Refactor API handler to use async/await

--- api/index.js
+++ api/index.js
@@ -1,18 +1,19 @@
-module.exports = (req, res) => {
-  const url = new URL(req.url, 'https://api.binance.com');
-  url.pathname = url.pathname.replace(/^\//, '');
-  
-  const { method, headers, body } = req;
-  
-  fetch(url.toString(), { method, headers, body })
-    .then(r => r.text())
-    .then(data => {
-      res.statusCode = r.status;
-      res.setHeader('Content-Type', 'application/json');
-      res.end(data);
-    })
-    .catch(e => {
-      res.statusCode = 500;
-      res.end(JSON.stringify({ error: e.message }));
-    });
+module.exports = async (req, res) => {
+  try {
+    const url = new URL(req.url, 'https://api.binance.com');
+    url.pathname = url.pathname.replace(/^\//, '');
+    
+    const { method, headers, body } = req;
+    
+    const response = await fetch(url.toString(), { method, headers, body });
+    const data = await response.text();
+    
+    res.statusCode = response.status;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(data);
+  } catch (e) {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ error: e.message }));
+  }
 };
